1ee2858c055d7071383673c72b7c7b5488ec5193
minjun <ivy0987@naver.com>
2021-06-24 15:44:04 +0900

    1st commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace FirstProj
+{
+
+    class Program
+    {
+        static int  numA = 99;
+        string chA = "abcde";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MinJun!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(numA + i);  
+            } 
+       Program proA = new Program
+       ProA doThat();
+        }
+  void doThat()
+  {
+        Console Writeline(cha
+  }
+    }
+}
